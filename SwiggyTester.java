8a19758267ada6ac40337aaa95032d6e2f7fd6c0
ManoharaAB17 <113663972+ManoharaAB17@users.noreply.github.com>
2022-09-22 23:15:40 +0530

    Delete SwiggyTester.java

--- SwiggyTester.java
+++
@@ -1,9 +0,0 @@
-class SwiggyTester{
- public static void main(String a[]){
-     //Swiggy.takeOrder("mosaranna") ; 
-   double ref = Swiggy.takeOrder("Burger",6 ) ; 
-   System.out.println(ref);
-   
-  System.out.println("Your Final Is Ready... Enjoyy");
-  }
-}
